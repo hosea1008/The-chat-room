2764ccbf2d6bc79c9306a30dfc146caf8811ce16
Hongshan Li <hongshan1008@163.com>
2020-05-09 16:07:32 +0800

    another trial failed because udt sockets uses some strange file descriptor...now turn to use tcp for command exchange

--- smalest_udt_client.py
+++ smalest_udt_client.py
@@ -3,6 +3,7 @@ import udt
 import logging
 import socket
 import threading
+import select
 import multiprocessing
 
 udt_socket = udt.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
@@ -13,13 +14,20 @@ udt_socket.connect(("127.0.0.1", 50011))
 # tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # tcp_socket.connect(("127.0.0.1", 50010))
 
+epoll = select.epoll()
+epoll.register(udt_socket.fileno(), udt.UDT_EPOLL_IN)
 
 def recv_data():
     logging.warning("receive thread started")
     while True:
-        data = udt_socket.recv(4, 0)
-        logging.warning("received data %s" % data)
-        # data = tcp_socket.recv(1024, 0)
+        events = epoll.poll(1)
+        for fileno, event in events:
+            if fileno == udt_socket.fileno():
+                logging.warning("fileno: %s, udt_socket.fileno: %s" % (fileno, udt_socket.fileno()))
+            elif event & select.EPOLLIN:
+                data = udt_socket.recv(1024, 0)
+                logging.warning("recv data: %s" % data)
+
 
 
 def printing():

f5693e8d00e7d65487756250a07d6bfb04672a0c
Hongshan Li <hongshan1008@163.com>
2020-05-09 15:13:49 +0800

    select and epoll seems not working here...trying to introduce tcp command channel to avoid this problem...

--- simplest_tcp_server.py
+++ simplest_tcp_server.py
@@ -2,18 +2,18 @@ import socket
 import udt
 import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 #
-# udt_s = udt.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+udt_s = udt.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 #
-s.bind(("127.0.0.1", 50010))
-s.listen(5)
-conn, addr = s.accept()
-#
-# udt_s.bind(("0.0.0.0", 50011))
-# udt_s.listen(5)
-# conn, addr = udt_s.accept()
+# s.bind(("127.0.0.1", 50010))
+# s.listen(5)
+# conn, addr = s.accept()
+
+udt_s.bind(("0.0.0.0", 50011))
+udt_s.listen(5)
+conn, addr = udt_s.accept()
 
 # conn.send(b"wake up neo...", 0)
 
--- smalest_udt_client.py
+++ smalest_udt_client.py
@@ -1,22 +1,25 @@
 import time
-
 import udt
 import logging
 import socket
 import threading
+import multiprocessing
 
-# udt_socket = udt.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-# udt_socket.connect(("127.0.0.1", 50011))
+udt_socket = udt.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+# udt_socket.setblocking(False)
+udt_socket.connect(("127.0.0.1", 50011))
 
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.connect(("127.0.0.1", 50010))
+
+# tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# tcp_socket.connect(("127.0.0.1", 50010))
 
 
 def recv_data():
+    logging.warning("receive thread started")
     while True:
-        # data = udt_socket.recv(1024, 0)
-        data = tcp_socket.recv(1024, 0)
-        logging.warning("recv data: %s" % data)
+        data = udt_socket.recv(4, 0)
+        logging.warning("received data %s" % data)
+        # data = tcp_socket.recv(1024, 0)
 
 
 def printing():
@@ -29,7 +32,8 @@ r_print = threading.Thread(target=printing)
 r_print.start()
 logging.warning("printing thread started...")
 
-
 r_recv = threading.Thread(target=recv_data)
 r_recv.start()
 logging.warning("receiving thread started...")
+
+r_recv.join()
